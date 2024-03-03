cece24c33c3fd596fe648770fde5e78c60eedd0c
Victor Martins <vmartins9292@gmail.com>
2024-03-03 16:26:55 -0300

    upload imagens

--- galeria/views.py
+++ galeria/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
-def index(requests):
-    return HttpResponse("<h1>Alura Space</h1><p>Bem vindo ao espaço</p>")
+def index(request):
+    return render(request, "galeria/index.html")
+
+
+def imagem(request):
+    return render(request, "galeria/imagem.html")
 
 
 # Create your views here.
